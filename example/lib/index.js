017b37d8f4d27cac0ee402b36833606299a96a8c
Ko1103 <ko.foot1103@gmail.com>
2019-11-30 19:10:08 +0900

    refrect gitignore

--- example/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ts_sample_1 = require("ts-sample");
-console.log(ts_sample_1.multiple(3, 2));
-//# sourceMappingURL=index.js.map
