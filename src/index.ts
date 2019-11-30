6d0f18c0c076082ad10da8fe787d4a9134360ea7
Ko1103 <ko.foot1103@gmail.com>
2019-11-30 18:39:51 +0900

    add new package

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function reduce(a: number, b: number): number {
+  return a + b;
+}
+
+export function multiple(a: number, b: number): number {
+  return a * b;
+}
+
+export function divide(a: number, b: number): number {
+  return Math.floor(a/b);
+}
